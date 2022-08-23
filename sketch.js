52be8d67a74f450e79e618c2b9e414712a70e82f
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2022-08-23 20:58:39 +0100

    added double to click to place double click in the canvas to place a flower using the currently set parameters

--- sketch.js
+++ sketch.js
@@ -1,62 +1,69 @@
-//global vars
+// global vars
 let colourPicker;
 
 function setup() {
+	frameRate(12);
+
 	createCanvas(800,600);
+	
 	colourPicker = createColorPicker('#c85afa');
 	colourPicker.position(0, height + 5);
-}
 
-function draw() {
-// background
-	background(90,190,230);
+	background(90, 190, 230);
 // ground
-	// ground height is customisable through the variable groundY
 	let groundY = 150
 	fill(120,230,70);
 	noStroke()
 	rect(0, height - groundY, width, groundY);
-	createFlower(width / 2, height - 20, 200, 0, 50, 15);
-	createFlower(width / 4, height - 30, 150, 0, 35, 5);
+}
+
+function draw() {
+}
+
+// double click to place flower
+function doubleClicked() {
+	createFlower(mouseX, mouseY, 150, 0, 35, 5);
 }
 
 function createFlower(x, y, h, n, s, w) {
-// creates a flower and places it on the coordinates specified 
-// where x and y are coordinates for base of the stem	
-// h is the height of the flower from base of stem to center of head
-// n is the number of petals around the flower
-// s is the size of the petals
-// w is the width of the stem
+/**
+ * creates a flower and places it on the coordinates specified 
+ * where x and y are coordinates for base of the stem	
+ * h is the height of the flower from base of stem to center of head
+ * n is the number of petals around the flower
+ * s is the size of the petals
+ * w is the width of the stem
+ */
 
 // o is the offset from the middle of the flower head
 // p is the main colour of the petals
 // c is the main colour of the centre
+
 	let o = (s / 2);
-	//let p = color(200, 90, 250);
 	let p = color(colourPicker.color());
 	let c = color(210, 200, 70);
-	//create stem with color #5AAF34
+	// create stem with color #5AAF34
 	strokeWeight(2);
 	stroke(50, 135, 12);
 	fill(70, 155, 32);
 	rect(x + (w / 2), y - h, w, h);
 
-	//create petals around flower
+	// create petals around flower
 	stroke(strokeColour(p));
 	fill(p);
-	//this isnt going to scale well for other flowers
+	// this isnt going to scale well for other flowers
 	ellipse(x + w - o, y - h + o, s);
 	ellipse(x + w + o, y - h - o, s);
 	ellipse(x + w - o, y - h - o, s);
 	ellipse(x + w + o, y - h + o, s);
 
-	//create centre of fower
+	// create centre of fower
 	strokeWeight(2);
 	stroke(strokeColour(c));
 	fill(c);
 	ellipse(x + w, y - h, s);
 }
-//using the same variable name as the createFlower function could cause confusion
+
 function strokeColour(c) { 
 // takes input colour c and returns the decreased value b
 // v is the value to decrease by
