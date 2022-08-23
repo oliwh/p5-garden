845b034f8e165cd6e3729407622a22a841fe2b28
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2022-08-23 10:11:46 +0100

    added canvas and ground

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-	
+	createCanvas(800,600);
 }
 
 function draw() {
-	
+// background
+	background(90,190,230);
+// ground
+	// ground height is customisable through the variable groundY
+	// the ground is created by spawning a rectangle always touching the bottom of the canvas
+	let groundY = 150
+	fill(120,230,70);
+	strokeWeight(0);
+	rect(0, height - groundY, width, groundY);
 }
